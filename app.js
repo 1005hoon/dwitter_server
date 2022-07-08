2675ae9c9b523ee99416b9ce7519e042dd9b9b79
hoon <hoonnotes@naver.com>
2022-07-08 11:48:54 +0900

    chore: add helmet and morgan for logging

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
+
+app.use(morgan("dev"));
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
